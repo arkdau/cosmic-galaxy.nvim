f6a0d8b8d60529fb921eed7ef718190b2bdf564c
Julia Daugielewicz <julia@pop-os.localdomain>
2026-06-19 12:15:57 +0200

    /home/julia/cosmic-galaxy.nvim/plugin/cosmic-galaxy.lua -  clear

--- plugin/cosmic-galaxy.lua
+++ plugin/cosmic-galaxy.lua
@@ -1,21 +1,21 @@
-if vim.g.loaded_cosmic_galaxy then
-  return
-end
-vim.g.loaded_cosmic_galaxy = true
+--if vim.g.loaded_cosmic_galaxy then
+--  return
+--end
+--vim.g.loaded_cosmic_galaxy = true
 
 -- automatyczne ładowanie highlightów po zmianie colorscheme
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = function()
-    local ok, galaxy = pcall(require, "cosmic-galaxy")
-    if ok then
-      local palette = require("cosmic-galaxy.palettes.cosmic_nvim")
-      galaxy.apply(palette, vim.api.nvim_set_hl)
-    end
-  end,
-})
+--vim.api.nvim_create_autocmd("ColorScheme", {
+--  pattern = "*",
+--  callback = function()
+--    local ok, galaxy = pcall(require, "cosmic-galaxy")
+--    if ok then
+--      local palette = require("cosmic-galaxy.palettes.cosmic_nvim")
+--      galaxy.apply(palette, vim.api.nvim_set_hl)
+--    end
+--  end,
+--})
 
 -- domyślne uruchomienie
-require("cosmic-galaxy").setup()
+--require("cosmic-galaxy").setup()
 
 
